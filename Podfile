0a35bb867a50b5d15988563c513b7a863b82da78
Jinyan Shao <jinyanshao@proton.me>
2026-09-02 10:24:11 +0200

    fix: make macOS tests reproducible

--- Podfile
+++ Podfile
@@ -17,7 +17,7 @@ post_install do |installer|
   installer.generated_projects.each do |project|
     project.targets.each do |target|
         target.build_configurations.each do |config|
-            config.build_settings['MACOSX_DEPLOYMENT_TARGET'] = 'RECOMMENDED_MACOSX_DEPLOYMENT_TARGET'
+            config.build_settings['MACOSX_DEPLOYMENT_TARGET'] = '13.5'
          end
     end
   end
--- unit-tests.sh
+++ unit-tests.sh
@@ -1,3 +1,17 @@
-xcodebuild clean -workspace PlumeFrancaise.xcworkspace/ -scheme Tests
+#!/usr/bin/env bash
 
-xcodebuild test CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -workspace PlumeFrancaise.xcworkspace/ -scheme Tests
+set -euo pipefail
+
+if [ ! -f "Pods/Target Support Files/Pods-PlumeFrancaise/Pods-PlumeFrancaise.debug.xcconfig" ]; then
+  pod install
+fi
+
+arch=$(uname -m)
+
+xcodebuild test \
+  CODE_SIGN_IDENTITY="" \
+  CODE_SIGNING_REQUIRED=NO \
+  ONLY_ACTIVE_ARCH=YES \
+  -workspace PlumeFrancaise.xcworkspace/ \
+  -scheme PlumeFrancaise \
+  -destination "platform=macOS,arch=${arch}"
